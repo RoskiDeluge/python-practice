f2eaef67fd46ee2773b38876a9b2d9b2fb0be139
Roberto Delgado <roberto.delgado@gmail.com>
2020-05-19 17:53:16 -1000

    function.py

--- function.py
+++ function.py
@@ -3,6 +3,28 @@
 def double(x):
     return x * 2
 
-num = double(10)
-
+num = 10
+num = double(num)
 print(num)
+
+# define a doubling function that doubles every element in a list
+# the argument is passed by reference
+
+def double_list(li):
+    # to make this immutable use list comprehension new_list = [element * 2 for element in li]
+    for i in range(0, len(li)):
+        li[i] *= 2
+    return li
+    # return new_list
+
+num_list = [1,2,3,4,5]
+print(num_list)
+double_list(num_list)
+print(num_list)
+
+# pass a collection by value
+import copy
+num_list = [1,2,3,4,5]
+print(num_list)
+num_list = double_list(copy.copy(num_list))
+print(num_list)
